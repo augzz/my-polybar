3317fd0043f0e14d06c7f7a8920d994e4da61fb3
Auggie <92235586+augzz@users.noreply.github.com>
2025-05-28 18:36:35 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Terminate already running bar instances
+# Terminate already running bar instances.
 polybar-msg cmd quit
 
+# Launch polybar on each open monitor.
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload main &
 done
