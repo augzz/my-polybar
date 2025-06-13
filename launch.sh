db53d67d00e9cdb469259e143f58cbeac1fc3e16
augzz <auggie.moore124@gmail.com>
2025-06-12 21:51:55 -0700

    v2: integrated with themes and pywal. Combined window toggle scripts into one. Added spotify player functionality.

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Terminate already running bar instances.
-polybar-msg cmd quit
-
-# Launch polybar on each open monitor.
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main &
-done
